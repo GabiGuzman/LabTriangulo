7ad7889977afb7566f13a151d9bf9e44f0eb1651
Guzman <Guzman@DESKTOP-29GVUO5>
2022-03-08 10:49:38 -0600

    Agregar archivos de proyecto.

---
+++ LabTriangulo/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Double
+        Dim b As Double
+        Dim c As Double
+        Dim d As Double
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = a * b
+        d = c / 2
+        TextBox3.Text = d
+
+    End Sub
+End Class
